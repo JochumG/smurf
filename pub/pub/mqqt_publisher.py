7c522be6d40250c25082b80fcd41f474b9104399
jochumg <jochum@myphp.nl>
2019-01-28 20:14:02 +0100

    smal

--- pub/pub/mqqt_publisher.py
+++ pub/pub/mqqt_publisher.py
@@ -1,19 +1,20 @@
 from flask import Flask
 import uwsgi
 
+import paho.mqtt.publish as publish
+MQTT_SERVER = "192.168.0.60"
+MQTT_PATH = "sensordata"
+publish.single(MQTT_PATH, "Hello publish local MQTTT!", hostname=MQTT_SERVER)
+print("published...")
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hey I'm using Docker AND MQQT! Az well"
 
-if __name__ == "__main__":
+if __name__ == "__mqqt_publisher__":
     app.run(host="0.0.0.0", debug=True, port=80)
 
 
 
-#import paho.mqtt.publish as publish
-#MQTT_SERVER = "192.168.0.60"
-#MQTT_PATH = "sensordata"
-#publish.single(MQTT_PATH, "Hello publish local MQTTT!", hostname=MQTT_SERVER)
-print("Stopped...")
