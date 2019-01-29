62048f41664dfdebd6fbe39f8b9b0cea71162834
jochumg <jochum@myphp.nl>
2019-01-29 11:15:14 +0100

    Dockerfile

--- pub/pub/mqqt_publisher.py
+++ pub/pub/mqqt_publisher.py
@@ -1,20 +1,20 @@
 
-#import uwsgi
+import uwsgi
 
-#import paho.mqtt.publish as publish
-#MQTT_SERVER = "192.168.0.60"
-#MQTT_PATH = "sensordata"
-#publish.single(MQTT_PATH, "Hello publish local MQTTT!", hostname=MQTT_SERVER)
+import paho.mqtt.publish as publish
+MQTT_SERVER = "192.168.0.60"
+MQTT_PATH = "sensordata"
+publish.single(MQTT_PATH, "Hello publish local MQTTT!", hostname=MQTT_SERVER)
 print("published...")
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route("/")
+@app.route("/")
 def hello():
     return "Hey I'm using Docker AND MQQT! Az well"
 
-#if __name__ == "__mqqt_publisher__":
-run(host="0.0.0.0", debug=True, port=80)
+if __name__ == "__mqqt_publisher__":
+    app.run(host="0.0.0.0", debug=True, port=80)
 
 
 
