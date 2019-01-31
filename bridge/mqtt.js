b2f447bb86c9524391dd48346fa65d4e191f4a7b
jochumg <jochum@myphp.nl>
2019-01-31 12:58:59 +0100

    node mqqt

---
+++ bridge/mqtt.js
@@ -0,0 +1,18 @@
+<script>
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqqt://77.248.61.13')
+ 
+client.on('connect', function () {
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
+</script>
